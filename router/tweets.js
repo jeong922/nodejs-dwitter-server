9999f35ffc7e814d745d63fb8431bb8a9e931b72
jeong922 <hwajeong922@naver.com>
2023-10-30 21:51:19 +0900

    Rest APIs - CRUD 구현

--- router/tweets.js
+++ router/tweets.js
@@ -1,6 +1,74 @@
 import express from 'express';
 import 'express-async-errors';
 
+let tweets = [
+  {
+    id: '1',
+    text: '❤❤❤❤❤',
+    createdAt: Date.now().toString(),
+    name: 'Bob',
+    username: 'bob',
+    url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
+  },
+  {
+    id: '2',
+    text: 'hi',
+    createdAt: Date.now().toString(),
+    name: 'Jeong',
+    username: 'jeong',
+  },
+];
 const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const username = req.query.username;
+  const data = username
+    ? tweets.filter((tweet) => tweet.username === username)
+    : tweets;
+
+  res.status(200).json(data);
+});
+
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `Tweet id(${id}) not found` });
+  }
+});
+
+router.post('/', (req, res, next) => {
+  const { text, name, username } = req.body;
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    createdAt: new Date(),
+    name,
+    username,
+  };
+
+  tweets = [tweet, ...tweets];
+  res.status(201).json(tweet);
+});
+
+router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const text = req.body.text;
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    tweet.text = text;
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `Tweet id(${id}) not found` });
+  }
+});
+
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id;
+  tweets = tweets.filter((tweet) => tweet.id !== id);
+  res.sendStatus(204);
+});
 
 export default router;
