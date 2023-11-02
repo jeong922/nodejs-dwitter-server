6f8c57e3e569e487bb08b49bae96fd9841a6f940
jeong922 <hwajeong922@naver.com>
2023-11-02 15:43:41 +0900

    가입, 로그인 Rest APIs 구현

---
+++ data/auth.js
@@ -0,0 +1,20 @@
+// abcd1234: $2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm
+let users = [
+  {
+    id: '1',
+    username: 'bob',
+    password: '$2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm',
+    name: 'Bob',
+    email: 'bob@gmail.com',
+    url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
+  },
+];
+export function findByUsername(username) {
+  return users.find((user) => user.username === username);
+}
+
+export function createUser(user) {
+  const created = { ...user, id: Date.now().toString() };
+  users.push(created);
+  return created.id;
+}
