135d546ef477bc98c69a70d69853f90f8d266f4f
jeong922 <hwajeong922@naver.com>
2023-11-13 21:04:33 +0900

    server - MongoDB 연결

---
+++ server/database/database.js
@@ -0,0 +1,7 @@
+import MongoDB from 'mongodb';
+import { config } from '../config.js';
+
+export async function connectDB() {
+  return MongoDB.MongoClient.connect(config.db.host) //
+    .then((client) => client.db());
+}
