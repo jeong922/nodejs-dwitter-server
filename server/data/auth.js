e4ce068a25d974748aa20a4c4c9e300d0e31c2e4
jeong922 <hwajeong922@naver.com>
2023-11-14 19:48:48 +0900

    server - tweet MongoDB 적용

--- server/data/auth.js
+++ server/data/auth.js
@@ -22,5 +22,5 @@ export async function createUser(user) {
 }
 
 function mapOptionalUser(user) {
-  return user ? { ...user, id: user._id } : null;
+  return user ? { ...user, id: user._id.toString() } : null;
 }
